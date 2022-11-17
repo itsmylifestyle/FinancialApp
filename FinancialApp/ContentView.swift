6fa834303804af1702a094af86e2bdd4e99c00ec
itsmylifestyle <hahaii00@mail.ru>
2022-11-17 15:56:09 +0600

    Initial Commit

---
+++ FinancialApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FinancialApp
+//
+//  Created by Айбек on 17.11.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FinancialApp/FinancialAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinancialAppApp.swift
+//  FinancialApp
+//
+//  Created by Айбек on 17.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct FinancialAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
